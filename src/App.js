a94b9e99b4943b4cc3b5f2384e7e0910463052a9
deanchen <antlet2004@gmail.com>
2017-06-13 10:19:15 +0800

    installed the chartjs component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import {Line} from 'react-chartjs';
+
+
+
 class App extends React.Component {
     render() {
-		return <div>hello wolrd</div>
+		const chartData = {
+			labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+			datasets: [{
+				label: '# of Votes',
+				data: [12, 19, 3, 5, 2, 3],
+				borderWidth: 1
+			}]
+		};
+		return <Line data={chartData} width='600' height='280' />
     }
 }
 
