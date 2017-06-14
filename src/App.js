0d5adc67903d77fa8844c9b850aa8dce511a10e5
deanchen <antlet2004@gmail.com>
2017-06-14 10:27:21 +0800

    add real fund data line, but the x axes label have problem

--- src/App.js
+++ src/App.js
@@ -5,16 +5,60 @@ import {Line} from 'react-chartjs';
 
 
 class App extends React.Component {
-    render() {
+	constructor(props){
+		super(props);
 		const chartData = {
-			labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-			datasets: [{
-				label: '# of Votes',
-				data: [12, 19, 3, 5, 2, 3],
-				borderWidth: 1
-			}]
 		};
-		return <Line data={chartData} width='600' height='280' />
+		this.state = { chartData};
+	}
+	
+	componentDidMount(){
+		fetch('http://localhost:9898/fundData')
+			.then(res => res.json())
+			.then(data => {
+				console.debug('data:',data);
+				if(data && data.length > 0){
+					data = data.reverse().map(d => (
+								{
+									fbrq :+(d.fbrq.replace(/-/g,'').replace(' 00:00:00','')),
+									jjjz :+d.jjjz,
+								}));
+					let labelsAll = data.map(d => d.fbrq);
+					let labels = labelsAll;
+					//let labels = [labelsAll[0]];
+					//for(let i = 1 ; i < 10;i++  ){
+					//	let l = Math.round((i/10) * labelsAll.length);
+					//	labels.push(labelsAll[l]);
+					//	console.debug('labels:',labels);
+					//}
+					//labels.push(labelsAll[labelsAll.length -1]);
+
+					const chartData = {
+						labels, //[data[data.length-1].fbrq,data[0].fbrq],
+						datasets: [{
+							label: 'the fund 1',
+							data : data.map(d => d.jjjz),
+							borderWidth: 1,
+							fill : false,
+						}]
+					};
+					console.debug('the chart data:',chartData);
+					this.setState({chartData});
+					this.forceUpdate();
+					
+				}
+			});
+	}
+
+
+    render() {
+		const options = {
+			title : {
+				display : true,
+				text : 'Fund Line',
+			},
+		}
+		return <Line data={this.state.chartData} options={options} redraw width='600' height='280' />
     }
 }
 
