a2fdae77faa3a079d29f3b874c0c0cd6256cfbf6
deanchen <antlet2004@gmail.com>
2017-07-02 15:50:34 +0800

    added flow support

--- src/App.js
+++ src/App.js
@@ -1,12 +1,44 @@
+// @flow
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import {Line,Bar} from 'react-chartjs';
-import {FundModel} from './doggie_model/src/FundModel.js';
+import {type FundData,FundModel} from './doggie_model/src/FundModel.js';
 
 
 
 class App extends React.Component {
-	constructor(props){
+	//property --------------------------------------
+	props: {
+	};
+	state: {
+		chartData : any,
+		chartData2 : any,
+		allYearEarning : any,
+		halfYearEarningAverage : any,
+		halfYearEarningRate : any,
+		fundNumber : any,
+		period : any,
+	};
+	fund : any;
+	period : any;
+	funds = {
+		'096001' : "大成标普500（096001）",
+		'519300' : "大成沪深300（519300）",
+		'050025' : '博时标普500ETF联接(050025)',
+		'159934' : '易方达黄金ETF(159934)',
+		'050002' : '博时沪深300指数A(050002)',
+		'161017' : '富国中证500指数增强(LOF)(161017)',
+		'162216' : '泰达宏利500指数分级(162216)',
+	};
+	periods = {
+		'183' : '半年',
+		'365' : '一年',
+		'730' : '两年',
+		'1095' : '三年',
+	};
+	
+	//method ----------------------------------------
+	constructor(props :any){
 		super(props);
 		const chartData2 = {
 			labels:[],
@@ -16,11 +48,24 @@ class App extends React.Component {
 				borderWidth: 1,
 			}]
 		};
-		this.state = { chartData:{},chartData2};
+		this.state = { 
+			chartData:{},
+			chartData2,
+			allYearEarning : null,
+			halfYearEarningAverage : null,
+			halfYearEarningRate : null,
+			fundNumber : '096001',
+			period : 365,
+		};
 	}
 	
 	componentDidMount(){
-		fetch('http://localhost:9898/fundData')
+		this.loadData();
+	}
+
+	loadData = () => {
+
+		fetch(`http://localhost:9898/fundData/${this.state.fundNumber}`)
 			.then(res => res.json())
 			.then(data => {
 				let model = new FundModel(); 
@@ -30,7 +75,7 @@ class App extends React.Component {
 								{
 									date :+(d.fbrq.replace(/-/g,'').replace(' 00:00:00','')),
 									jjjz :+d.jjjz,
-								}));
+								} : FundData));
 					let labelsAll = data.map(d => d.date);
 					let labels = labelsAll;
 					const chartData = {
@@ -43,14 +88,14 @@ class App extends React.Component {
 						}]
 					};
 					console.debug('the chart data:',chartData);
-					let allYearEarning = model.calculatePeriodEarning(data,data[0].date,8*365) * 100 + '%' ;
+					let allYearEarning = model.calculatePeriodEarning(data,data[0].date,8*this.state.period) * 100 + '%' ;
 					console.debug(`all year earning:${allYearEarning},begin:${data[0].date}`);
 					this.setState({allYearEarning});
 					//half year earning calculate
 					let earningList = [];//{date:xxxx,earning:0.01}
 					{
 						data.forEach(dayObj => {
-							let earning = model.calculatePeriodEarning(data,dayObj.date,182);
+							let earning = model.calculatePeriodEarning(data,dayObj.date,this.state.period);
 							earningList.push({date:dayObj.date,earning});
 						});
 					}
@@ -89,6 +134,18 @@ class App extends React.Component {
 			});
 	}
 
+	changeFund = () => {
+		console.info('the fund selected : ',this.fund.value);
+		this.setState({fundNumber : this.fund.value},()=>{
+			this.loadData();
+		});
+	}
+	changePeriod = () => {
+		console.info('the period selected : ',this.period.value);
+		this.setState({period : this.period.value},()=>{
+			this.loadData();
+		});
+	}
 
     render() {
 		const options = {
@@ -97,12 +154,26 @@ class App extends React.Component {
 				text : 'Fund Line',
 			},
 		}
+		const os = [];
+		for(let k in this.funds){
+			os.push({value:k,name:this.funds[k]});
+		}
+		const osPeriod = [];
+		for(let k in this.periods ) {
+			osPeriod.push({value:k,name : this.periods[k]});
+		}
 		return (
 			<div>
-				<p>大成500基金净值趋势</p>
+				<select ref={r => this.fund = r} onChange={this.changeFund} value={this.state.fundNumber}  >
+					{os.map( o => <option value={o.value}  >{o.name}</option>)}
+				</select>
+				<select ref={r => this.period = r} onChange={this.changePeriod} value={this.state.period}  >
+					{osPeriod.map( o => <option value={o.value}  >{o.name}</option>)}
+				</select>
+				<p>{this.funds[this.state.fundNumber]}净值趋势</p>
 				<Line data={this.state.chartData} options={options} redraw width='2048' height='500' />
 				<p>总收益率:{this.state.allYearEarning}</p>
-				<p>大成500基金净值半年投资收益</p>
+				<p>{this.funds[this.state.fundNumber]}净值半年投资收益</p>
 				<Bar data={this.state.chartData2} options={options} redraw width='8000' height='500' />
 				<p>平均收益率:{this.state.halfYearEarningAverage}</p>
 				<p>盈利概率：{this.state.halfYearEarningRate}</p>
